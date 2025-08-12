acafeb297dc6b11628328292b176430aa69e17c1
Lars87-maker <larsstevens@hotmail.com>
2025-08-12 13:35:32 +0200

    Delete src/main.tsx

--- src/main.tsx
+++
@@ -1,7 +0,0 @@
-
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './styles/index.css'
-import App from './modules/App'
-createRoot(document.getElementById('root')!).render(<React.StrictMode><BrowserRouter><App/></BrowserRouter></React.StrictMode>)
